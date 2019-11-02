67e4e300a78ca06f1bce1f6f8865c4cb0191f155
Roman <roman@ototak.net>
2019-11-02 13:02:35 +0100

    Fix readme and test names

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ Small, fast, dependencies free helper for BEM class name generation.
 ```js
 import bem from 'boombem';
 
-const cn = bem('Component');
-console.log( cn() ); // Component
-console.log( cn('Element') ); // Component__Element
+const cn = bem('Block');
+console.log( cn() ); // Block
+console.log( cn('Element') ); // Block__Element
 console.log( cn('Element', {
     a: true,
     b: false,
     c: 0,
     d: {},
     e: '1'
-})); // Component__Element Component__Element--a Component__Element--d Component__Element--e
+})); // Block__Element Block__Element--a Block__Element--d Block__Element--e
 ```
--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,10 @@
 import bem from '..';
 
-describe('reabem', () => {
+describe('boombem', () => {
     it('should work', () => {
-        const cn = bem('Component');
-        expect( cn() ).toEqual('Component');
-        expect( cn('Element') ).toEqual('Component__Element');
-        expect( cn('Element', {a: true, b: false, c: 0, d: {}, e: '1'}) ).toEqual('Component__Element Component__Element--a Component__Element--d Component__Element--e');
+        const cn = bem('Block');
+        expect( cn() ).toEqual('Block');
+        expect( cn('Element') ).toEqual('Block__Element');
+        expect( cn('Element', {a: true, b: false, c: 0, d: {}, e: '1'}) ).toEqual('Block__Element Block__Element--a Block__Element--d Block__Element--e');
     });
 });
